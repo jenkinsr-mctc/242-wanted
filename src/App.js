b5f2acafaf7f92db14732e7a66209ee4bfe93a66
Rob Jenkins <jenkinr@mctc.edu>
2021-02-25 10:07:13 -0500

    Import change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit Me and save to reload.
+          Edit Me.
         </p>
         <a
           className="App-link"
